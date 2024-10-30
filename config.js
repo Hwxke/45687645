7f6fd94620534e730c2685746dd709b0436409d3
Hwxke <92125920+Hwxke@users.noreply.github.com>
2024-10-30 05:06:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,14 +21,14 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1300636316240973924", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  activityName: "V2 by Hxk", // This is bot status Write Anything here 
+  activityType: "playing",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/LCDAS",
   CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
   MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
@@ -59,10 +59,8 @@ module.exports = {
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
+║  ## Created by Hxk!                                                    ║
 ║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
