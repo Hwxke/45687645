9fa85065e636330455d3d1a1d487e9d18485c05d
Hwxke <92125920+Hwxke@users.noreply.github.com>
2024-10-30 05:18:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -27,26 +27,12 @@ module.exports = {
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
   activityName: "V2 by Hxk", // This is bot status Write Anything here 
-  activityType: "playing",  // Available activity types : LISTENING , PLAYING
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/LCDAS",
   CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
   MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
   errorLog: "", 
-
-   // Lavalink Server Details
-
-  nodes: [
-    {
-        name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
-        reconnectTimeout: 5000,
-        reconnectTries: Infinity,
-        secure: false
-    },
- ]
 }
 /*
 
